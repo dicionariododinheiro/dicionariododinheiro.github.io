15984a8b68b5d147d1d2ef1ed4faa5042779c140
dicionariododinheiro <odicionariododinheiro@gmail.com>
2026-08-18 23:55:15 -0300

    content

--- js/cards.js
+++ js/cards.js
@@ -19,8 +19,189 @@ const cards = [
             {
                 tipo: "texto",
                 imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
-                texto: "Outro texto aqui..."
-            }
+                texto: "serve para bancar os custos de manutenção e benefícios do cartão..."
+            }, 
+
+            {   
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "a anuidade pode ser cobrada de uma vez ou todo mês..."
+            },
+
+            {
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "Você não precisa disso, hoje em dia muitos cartões não cobram anuidade..."
+            },
+
+            {
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "Você pode ligar no banco e negociar um desconto ou a isenção dessa taxa..."
+            },
+
+            {
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "Só pague se os benefícios do cartão compensarem o valor da anuidade"
+            },
+
+        ]
+    }
+
+    {
+        titulo: "[cartão de crédito]",
+
+        slides: [
+            {   
+                tipo: "capa",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                subtitulo: "O que é",
+                titulo: "benefício do cartão?"
+            },
+
+            {   
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "Muitos cartões oferecem vantagens gratuitas para atrair e fidelizar clientes."
+            },
+
+            {
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "como descontos exclusivos em cinemas, shows e lojas parceiras."
+            },
+
+            {
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "Há também o acúmulo de milhas para trocar por passagens aéreas."
+            },
+
+            {
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "Mas o queridinho do momento é receber cashback a cada compra."
+            },
+            
+        ]
+    }
+
+    {
+        titulo: "[cartão de crédito]",
+
+        slides: [
+            {   
+                tipo: "capa",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                subtitulo: "O que é",
+                titulo: "ganhar cashback?"
+            },
+
+            {   
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "É um benefício oferecido por bancos, cartões de crédito e lojas parceiras."
+            },
+
+            {
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "Cashback significa literalmente "dinheiro de volta" em inglês."
+            },
+
+            {
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "ao comprar algo, uma parte do valor pago retorna para o seu bolso."
+            },
+
+            {
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "Esse dinheiro pode voltar como saldo na conta ou desconto na fatura.."
+            },
+        ]
+    }
+
+    {
+        titulo: "[cartão de crédito]",
+
+        slides: [
+            {   
+                tipo: "capa",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                subtitulo: "O que é",
+                titulo: "comprar à vista?"
+            },
+
+            {   
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "Significa pagar o valor total do produto no momento da compra."
+            },
+
+            {
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "Pode ser feito em dinheiro, Pix, no cartão de débito ou em 1x no crédito."
+            },
+
+            {
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "Pagar à vista evita que você comprometa sua renda dos próximos meses."
+            },
+
+            {
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "Além disso, você pode conseguir bons descontos com o vendedor."
+            },
+
+            {
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "Junte o dinheiro antes de comprar e sinta a liberdade de não ter parcelas!"
+            },
+
+        ]
+    }
+
+    {
+        titulo: "[cartão de crédito]",
+
+        slides: [
+            {   
+                tipo: "capa",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                subtitulo: "O que é",
+                titulo: "parcelar a compra?"
+            },
+
+            {   
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "É dividir o preço de uma compra em vários pedaços ao longo dos meses."
+            },
+
+            {
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "ajuda a comprar itens caros sem gastar todo seu dinheiro de uma vez..."
+            },
+
+            {
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "mas, se o parcelamento tiver juros, você vai pagar mais caro pelo mesmo item"
+            },
+
+            {
+                tipo: "texto",
+                imagem: "images/cartao-de-credito/anuidade-cartao_01.png",
+                texto: "Atenção: várias pequenas parcelas podem virar uma fatura enorme no futuro."
+            },
         ]
     }
 ];
