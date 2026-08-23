dacf4e2140a68e5d1a58187ebd8c7e825a4b945e
dicionariododinheiro <odicionariododinheiro@gmail.com>
2026-08-23 14:43:22 -0300

    cards

--- js/cards.js
+++ js/cards.js
@@ -35,5 +35,43 @@ const cards = [
         `
       }
     ]
+  },
+
+  {
+    number: "02",
+
+    cover: {
+      image: "",
+      smallText: "eu tenho",
+      title: [
+        "benefícios",
+        "no cartão?"
+      ]
+    },
+
+    slides: [
+      {
+        title: "anuidade do cartão",
+
+        text: `
+          É uma taxa cobrada pelo
+          banco apenas para você ter
+          o cartão de crédito e serve
+          para bancar custos de
+          manutenção e benefícios...
+        `
+      },
+
+      {
+        title: "benefícios",
+
+        text: `
+          Alguns cartões oferecem
+          benefícios como cashback,
+          pontos, milhas e acesso
+          a salas VIP.
+        `
+      }
+    ]
   }
 ];
